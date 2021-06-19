e813b9dd05f146930aef5feb17a0b0781e278e8f
최원혁 <whchoi000@gmail.com>
2021-06-19 20:31:04 +0900

    deploy

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+    apiKey: "AIzaSyCkO47r4YeW7NRdJbtccD4tkUPiDOBPzfM",
+    authDomain: "gdsc-hufs.firebaseapp.com",
+    projectId: "gdsc-hufs",
+    storageBucket: "gdsc-hufs.appspot.com",
+    messagingSenderId: "178307825244",
+    appId: "1:178307825244:web:5c6408d273741e4df1b099",
+    measurementId: "G-FXT9Y1CBFK"
 }
